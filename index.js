1130608b47a1d846e605e0887068197e602bb005
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 18:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}`
 }
+function holidayCountdown(holiday, numOfDays){
+  return `It's ${numOfDays} days until ${holiday}!`
+}
